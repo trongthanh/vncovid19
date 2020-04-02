e29f255a01188f4319a8874c0c8c226d9bf5dbf3
Thanh Tran <thanhtran@chotot.vn>
2020-04-02 13:24:49 +0700

    fix validator not exit non-zero when fail; fix validate errors

--- scripts/validate.js
+++ scripts/validate.js
@@ -6,4 +6,7 @@ const validate = ajv.compile(require('../data/patients-schema.json'));
 const patientsJson = require('../data/patients.json');
 
 if (validate(patientsJson)) console.log('patients.json OK');
-else console.log('Errors:', validate.errors);
+else {
+	console.log('Errors:', validate.errors);
+	process.exit(1);
+}

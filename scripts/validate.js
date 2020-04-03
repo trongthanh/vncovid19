de13a1aa67746f1c423150638eebfa1e06efea1d
Thanh Tran <thanhtran@chotot.vn>
2020-04-03 15:43:06 +0700

    validating the meta total in patients.json

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,26 @@
 const Ajv = require('ajv');
 
 const ajv = new Ajv();
-const validate = ajv.compile(require('../data/patients-schema.json'));
+const validatePatientsSchema = ajv.compile(require('../data/patients-schema.json'));
 
 const patientsJson = require('../data/patients.json');
 
-if (validate(patientsJson)) console.log('patients.json OK');
+function validatePatientsData(patientsJson) {
+	if (validatePatientsSchema(patientsJson)) {
+		// check total
+		const patientIds = Object.keys(patientsJson.data);
+		if (patientIds.length !== patientsJson.total) {
+			console.error(`Errors: meta total expect to be ${patientIds.length}`);
+			return false;
+		}
+		return true;
+	}
+
+	console.error('Errors:', validatePatientsSchema.errors);
+	return false;
+}
+
+if (validatePatientsData(patientsJson)) console.log('patients.json OK');
 else {
-	console.log('Errors:', validate.errors);
 	process.exit(1);
 }
